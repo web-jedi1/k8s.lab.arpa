729bf08aed2d3b1491fd05472aaa8692ff5cd77b
manfred <web_jedi1@protonmail.ch>
2025-01-20 10:02:10 +0100

    change tf provider version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,8 @@
 terraform {
-    required_version = ">= 1.1.0"
     required_providers {
         proxmox = {
-            source  = "telmate/proxmox"
-            version = "2.9.5"
+            source  = "Telmate/proxmox"
+            version = "3.0.1-rc6"
         }
     }
 }
