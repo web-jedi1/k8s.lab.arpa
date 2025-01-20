45661fe22d9ffeb869c770120e568781cc59f666
manfred <web_jedi1@protonmail.ch>
2025-01-20 09:45:48 +0100

    add k8s master

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
 resource "proxmox_vm_qemu" "k8s-master" {
 
-  name = "arpa-k8s-master-01" # Name to call the VM
-  desc = "Kubernetes Master" # Description for the VM
-  target_node = var.proxmox_host # Proxmox target node
+  name = "arpa-k8s-master-01"
+  desc = "Kubernetes Master"
+  target_node = var.proxmox_host 
 
-  clone = var.template_name  # The name of the template that this resource will be created from
+  clone = var.template_name 
 
-  agent = 1 # is the qemu agent installed?
+  agent = 1
 
-  cores = 2 # number of CPU cores
-  sockets = 1 # number of CPU sockets
-  cpu = "host" # The CPU type
-  memory = 2048 # Amount of memory to allocate
-  onboot = false # start the VM on host startup
+  cores = 1
+  sockets = 1
+  cpu_type = "host"
+  memory = 2048 
+  onboot = false
   os_type = "cloud-init"
 
-  # if you want two NICs, just copy this whole network section and duplicate it
   network {
+    id = 0
     model = "virtio"
     bridge = "vmbr1"
     tag = 2003
   }
 
-  ipconfig0 = "ip=10.0.3.2,gw=10.0.3.1"
+  ipconfig0 = "ip=10.0.3.10,gw=10.0.3.1"
   nameserver = "10.0.2.2"
   searchdomain = "lab.arpa"
 
@@ -31,19 +31,17 @@ resource "proxmox_vm_qemu" "k8s-master" {
     type = "socket"
   }
 
-  # sshkeys set using variables. the variable contains the text of the key.
   sshkeys = <<EOF
   ${var.ssh_pub_key}
   EOF
 
-  # Terraform has provisioners that allow the execution of commands / scripts on a local or remote machine.
   provisioner "remote-exec" {
-    inline = ["sudo apt update", "sudo apt upgrade -y", "echo Done!"]
+    inline = ["sudo apt update", "sudo apt upgrade -y", "sudo apt -y autoremove"]
 
     connection {
-      host        = "10.0.3.2"
+      host        = "10.0.3.10"
       type        = "ssh"
-      user        = "ubuntu"
+      user        = "manfred"
       private_key = file(var.private_key_path)
     }
   }
